44781bd43f0b4a7d8d6134387e7c738f68169b0c
cerbn <ru.quintupil@duocuc.cl>
2024-11-21 18:43:28 -0300

    ahora siii

--- capstone/settings.py
+++ capstone/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['polonort.netlify.app']
 
 
 # Directorios adicionales donde buscar archivos estáticos
