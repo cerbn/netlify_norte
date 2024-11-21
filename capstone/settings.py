3c3edb39a2e1b24f039711432569acbde1682605
cerbn <ru.quintupil@duocuc.cl>
2024-11-21 19:08:14 -0300

    holamundo

--- capstone/settings.py
+++ capstone/settings.py
@@ -77,7 +77,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'capstone.urls'
 
